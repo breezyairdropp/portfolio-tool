d2caa3ad4a0ecf20db5fb5741bc85b41f8c8650c
breezyairdropp <breezyairdrop@gmail.com>
2024-06-12 05:13:23 +0100

    Update leads.tsx

--- pages/leads.tsx
+++ pages/leads.tsx
@@ -415,6 +415,7 @@ const Leads: NextPage = () => {
         </div>
         <br />
         <br />
+        <button onclick={getQueryResult}>getResult</button>
         <div className="w-full max-w-1xl sm:max-w-2xl">
           <div className="w-full max-w-1xl sm:max-w-2xl">
             <div id="pendingFetch">
